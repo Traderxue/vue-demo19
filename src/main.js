19b474db5f8f09e07360573107c84c721b0df859
Traderxue <falethe@gmail.com>
2024-01-08 14:23:49 +0800

    init

--- src/main.js
+++ src/main.js
@@ -7,8 +7,13 @@ import App from './App.vue'
 import router from './router'
 import vant from 'vant';
 import 'vant/lib/index.css';
+import nutui from "@nutui/nutui-bingo";
+import "@nutui/nutui-bingo/dist/style.css";
+
 
 const app = createApp(App)
+
+app.use(nutui)
 
 app.use(vant)
 
